995b869dd1cef3607469cb0950e5b64f750dd4c6
franco-ednaldo <ednaldofranco.dev@gmail.com>
2023-08-20 18:11:02 -0300

    first commit

---
+++ FirstChallenge.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let firstName = "Steve"
+var lastName:String? = "Jobs"
+print("\(firstName) \(lastName ?? "Wozniak")")
+
+if let lastName {
+    print("\(firstName) \(lastName)")
+}
+
